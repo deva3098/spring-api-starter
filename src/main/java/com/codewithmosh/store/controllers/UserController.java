0f5c6bdd681420c550d0e83ade793536d10136de
devanshu <devanshug678@gmail.com>
2026-04-17 01:25:34 +0530

    setting http status codes

--- src/main/java/com/codewithmosh/store/controllers/UserController.java
+++ src/main/java/com/codewithmosh/store/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.entities.User;
 import com.codewithmosh.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,11 @@ public class UserController {
         return userRepository.findAll();
     }
     @GetMapping("/{id}")
-    public User findUser(@PathVariable Long id){
-        return userRepository.findById(id).orElseThrow(null);
+    public ResponseEntity<User> findUser(@PathVariable Long id){
+        var user = userRepository.findById(id).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
