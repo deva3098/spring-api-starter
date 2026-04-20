4d84f225bdd6a809509614cee4c128614ce705b9
devanshu <devanshug678@gmail.com>
2026-04-20 22:06:57 +0530

    Customizing Response Data

--- src/main/java/com/codewithmosh/store/dtos/UserDto.java
+++ src/main/java/com/codewithmosh/store/dtos/UserDto.java
@@ -1,7 +1,14 @@
 package com.codewithmosh.store.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
@@ -9,4 +16,6 @@ public class UserDto {
     private Long id;
     private String name;
     private String email;
+    @JsonFormat(pattern = "yyyy-MM-DD HH-MM-SS")
+    private LocalDateTime createdAt;
 }
